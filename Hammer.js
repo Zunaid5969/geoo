88973edad5c9ad806f4696e68939293fc3d2fcad
Zunaid5969 <74778429+Zunaid5969@users.noreply.github.com>
2021-06-18 20:47:31 +0530

    Update Hammer.js

--- Hammer.js
+++ Hammer.js
@@ -20,8 +20,8 @@ class Hammer {
     translate(pos.x, pos.y);
     rotate(angle);
     strokeWeight(3);
-    stroke('white')
-    fill('orange')
+    stroke("white")
+    fill("orange")
     rectMode(CENTER)
     rect(0, 0, this.width, this.height);
     pop();
