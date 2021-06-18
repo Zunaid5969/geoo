6edae7dc5d8134dbe71e933c5d4cf3700db99a6c
Zunaid5969 <74778429+Zunaid5969@users.noreply.github.com>
2021-06-18 20:20:22 +0530

    Update iron.js

--- iron.js
+++ iron.js
@@ -5,20 +5,20 @@ class iron{
          density:30,
          friction:3
         }
-       this.body=Bodies.rectangle(x,y,50,50,options);
-       this.width=50;
-       this.height=50;
+       this.body=Bodies.rectangle(x,y,width,height,options);
+       this.width=width;
+       this.height=height;
        World.add(world,this.body);
     }
     display(){
      var p=this.body.position;
-     var ang=this.body.angle;
+    // var ang=this.body.angle;
 
      push();
      translate(p.x,p.y);
-     rotate(angle);
-    // strokeWeight(3);
-    // stroke('white');
+     //rotate(angle);
+     strokeWeight(3);
+    stroke('white');
         rectMode(CENTER);
      fill('red');
     rect(0,0,this.width,this.height);
